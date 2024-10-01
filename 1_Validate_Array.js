abb7a7ae4d2cffa6e206ad288b1237cd77260a76
MiguelMPinto <pinto.miguel@outlook.pt>
2024-10-01 11:15:17 +0100

    Pergunta 1 Terminada

--- 1_Validate_Array.js
+++ 1_Validate_Array.js
@@ -1,16 +1,10 @@
 
-a= 0
 function validateArrayElements(arr, elementValidator){
     for (i in arr){
         for ( let j of elementValidator.validators){
-            if (j(i)){
-             a++
-             console.log(a)
-            return false
-        }
-        }
-        return true
-    };
+            if (!j(arr[i])){
+                return false}}};
+    return true
 }
 
 
@@ -31,3 +25,38 @@ const elementValidator = {
   console.log(validateArrayElements(arr1, elementValidator)); // true
   console.log(validateArrayElements(arr2, elementValidator)); // false
   console.log(validateArrayElements(arr3, elementValidator)); // false
+
+  // Example2
+
+const stringValidator = {
+    validators: [
+      (str) => typeof str === 'string',      // checks if the element is a string
+      (str) => str.length > 3,               // checks if the string has more than 3 characters
+    ],
+  };
+  
+  const strArr1 = ['hello', 'world', 'test']; // all strings, length > 3
+  const strArr2 = ['hi', 'ok', 'world'];      // 'hi' and 'ok' have length <= 3
+  const strArr3 = [123, 'abc', 'def'];        // contains non-string elements
+  
+  console.log(validateArrayElements(strArr1, stringValidator)); // true
+  console.log(validateArrayElements(strArr2, stringValidator)); // false
+  console.log(validateArrayElements(strArr3, stringValidator)); // false
+
+
+  // Example 3
+const mixedValidator = {
+    validators: [
+      (val) => typeof val === 'number',      // checks if the element is a number
+      (val) => val >= 10 && val <= 100,      // checks if the number is between 10 and 100
+    ],
+  };
+  
+  const mixedArr1 = [15, 25, 50, 100];      // all numbers between 10 and 100
+  const mixedArr2 = [5, 30, 50, 200];       // contains numbers outside the range
+  const mixedArr3 = [20, 'hello', 50];      // contains non-number elements
+  
+  console.log(validateArrayElements(mixedArr1, mixedValidator)); // true
+  console.log(validateArrayElements(mixedArr2, mixedValidator)); // false
+  console.log(validateArrayElements(mixedArr3, mixedValidator)); // false
+  
